6a5d681a3bf6685faab8d3019c946da633f7d3ed
xildar98 <xildar98@mail.ru>
2018-11-18 01:00:43 +0300

    class structure of project and example

---
+++ era/cmake-build-debug/CMakeFiles/era.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/era.dir/main.cpp.o"
+  "era.pdb"
+  "era"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/era.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
